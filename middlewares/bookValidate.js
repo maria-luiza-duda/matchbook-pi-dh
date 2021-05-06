1fa345834e77c80fc40cdc2754682ea0da2ffe90
maria-luiza-duda <marialuizaduda00@gmail.com>
2021-05-06 09:20:26 -0300

    retirando UM ESPAÇO no book validate

--- middlewares/bookValidate.js
+++ middlewares/bookValidate.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
     let { name, author, description, publisher } = req.body;
 
 
-    if (!name || !author || !description || !publisher ) {
+    if (!name || !author || !description || !publisher) {
         res.status(400).json({ erro: "Required field" });
     } else {
 
