a89aab663cfc4e890130876e209c738a6cc52cda
maria-luiza-duda <marialuizaduda00@gmail.com>
2021-05-06 09:14:07 -0300

    deletar arquivos desnecessarios

--- routes/coverRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const coverController = require('../Controller/coverController');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', coverController.index)
-router.post('/uploadcover', coverController.uploadcover);
-
-module.exports = router;
