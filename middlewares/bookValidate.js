ae74037cf82f74c52e924b4721f4951fd2ab93e9
Maria Luiza Duda <marialuizaduda00@gmail.com>
2021-05-06 00:16:28 -0300

    Update bookValidate.js

--- middlewares/bookValidate.js
+++ middlewares/bookValidate.js
@@ -2,10 +2,10 @@ const { Book, sequelize } = require('../models');
 
 module.exports = async (req, res, next) => {
 
-    let { name, author, publisher, generes, description } = req.body;
+    let { name, author, publisher,, description } = req.body;
 
 
-    if (!name || !author || !publisher || !generes || !description ) {
+    if (!name || !author || !publisher || !description ) {
         res.status(400).json({ erro: "Required field" });
     } else {
 
